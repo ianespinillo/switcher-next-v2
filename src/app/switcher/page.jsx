96e898361d1af94f175afb3865fb396beb1862d8
Ian Espinillo <ianespinillo@gmail.com>
2024-03-25 08:58:07 -0300

    fix

--- src/app/switcher/page.jsx
+++ src/app/switcher/page.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import { SubsCard } from '@/components/cards/card'
 import { subs } from '@/Data/subscriptions'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/lib/authoptions'
+
 export default async function Switcher_subs () {
-  const session = await getServerSession(authOptions)
-  (session)
+
   return (
     <div className='flex flex-wrap justify-center items-center min-h-[calc(100vh-62px)] h-full w-full gap-4'>
       {subs.map((sub, i) => (
